671a8e879755934b1b70a23df15f385e3466fefa
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-26 08:41:26 -0300

    refactor(css): centralize and disable scroll

--- src/components/countdown/Countdown.js
+++ src/components/countdown/Countdown.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState} from 'react'
+import styled from "@emotion/styled"
+// Utils
 import { getTimeLeft } from "./date-evaluation"
+
+const Container = styled.div`
+    position: fixed;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    top: 0;
+    left: 0;
+`
+
+const CountdownWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
 
 export const Countdown = () => {
     const [timeLeft, setTimeLeft] = useState(getTimeLeft("09/17/2020"));
@@ -28,15 +47,15 @@ export const Countdown = () => {
     });
 
     return (
-        <div>
+        <Container>
             {
                 timerComponents.length ? (
-                    <div style={{ height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                    <CountdownWrapper>
                         <span>{timerComponents}</span>
                         <span>Soon your surprise will be available!</span>
-                    </div>
+                    </CountdownWrapper>
                 ) : <span>Time's up!</span>
             }
-        </div>  
+        </Container>  
     );
 }

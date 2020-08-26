ae8fe9a9222402f8dc70e42aec80cf144918ec6f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-26 08:33:05 -0300

    feat(css): add global reset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import emotionReset from 'emotion-reset';
+import {Global, css} from '@emotion/core';
 // UI Elements
 import { Countdown } from "./components/countdown"
 
-export const App = () => <Countdown />
+export const App = () => (
+  <>
+    <Global styles={css`
+        ${emotionReset}
+
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+      `} 
+    />
+    <Countdown />
+  </>
+)
 

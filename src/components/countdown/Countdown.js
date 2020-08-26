dd4080d65a66caa9eec41a06ce7dc8e4b777384c
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-26 14:09:23 -0300

    fix: adjust mobile font size

--- src/components/countdown/Countdown.js
+++ src/components/countdown/Countdown.js
@@ -4,6 +4,7 @@ import styled from "@emotion/styled"
 import { Heartbeat } from '../heartbeat'
 // Utils
 import { getTimeLeft, buildTimePieces } from "./datetime-utils"
+import { MediaQueries } from '../../styles'
 
 const Container = styled.div`
     position: fixed;
@@ -15,6 +16,10 @@ const Container = styled.div`
     left: 0;
     background-color: #FDD5C8;
     color: #F9476C;
+    font-size: 18px;
+    ${MediaQueries.mobile} {
+        font-size: 16px;
+    }
 `
 
 const CountdownWrapper = styled.div`
@@ -33,10 +38,12 @@ const SurpriseSpoilerWrapper = styled.div`
         margin-right: 5px;
         font-weight: 800;
     }
+    ${MediaQueries.mobile} {
+        font-size: 13px;
+    }
 `
 
 const Timer = styled.span`
-    font-size: 18px;
     font-weight: 800;
     margin-top: 1rem;
 `
